945bf0f8372924386b5a57bc2cd8f801a1c089ef
CrushChen <maocc96@126.com>
2019-04-16 11:03:22 -0600

    first commit
    Using jQuery to build a ToDoList App

---
+++ assets/js/todos.js
@@ -0,0 +1,31 @@
+// check off specific todos by clicking 
+$("ul").on("click","li",function()
+{
+$(this).toggleClass("completed");
+});
+
+
+//click X to delete 
+$("ul").on("click","span",function(event)
+{
+  $(this).parent().fadeOut(function()
+  {
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event)
+{
+    if(event.which ===13)
+    {
+       var  newtodo =  $(this).val();
+       $(this).val("");
+       // create a new li and add it to ul 
+       $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> "+newtodo+"</li>");
+    }
+})
+$(".fa-plus").click(function()
+{
+    $("input[type='text").fadeToggle();
+})
